1216a88d8a89ff08b4be8b65ab34ea2ed9e9bc5b
Pranav Krishna <pranavkrishna.email@gmail.com>
2025-01-07 02:50:22 +0530

    Integrated Class Based Views

--- trading_system/trading/urls.py
+++ trading_system/trading/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'trading'  
 
 urlpatterns = [
-    path('', views.LoginView.as_view(), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='trading/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('home/', views.HomeView.as_view(), name='home'),
     path('orderbook/', views.OrderBookView.as_view(), name='orderbook'),
